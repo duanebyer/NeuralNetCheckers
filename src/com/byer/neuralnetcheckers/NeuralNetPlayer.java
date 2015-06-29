61102453608faf9fbcf81cfe4739ac36a9ab04c6
duane.byer <duane.byer@gmail.com>
2015-06-29 17:54:07 -0400

    Finished the NeuralNetPlayer

--- src/com/byer/neuralnetcheckers/NeuralNetPlayer.java
+++ src/com/byer/neuralnetcheckers/NeuralNetPlayer.java
@@ -2,6 +2,7 @@ package com.byer.neuralnetcheckers;
 
 import com.asemahle.neuralnet.NeuralNet;
 import com.byer.checkers.Board;
+import com.byer.checkers.PieceType;
 import com.byer.checkers.Player;
 import java.util.List;
 
@@ -18,20 +19,82 @@ public class NeuralNetPlayer extends Player {
     @Override
     public Board getMove(Board board, boolean isWhite) {
         List<Board> possibleMoves = board.getPossibleMoves(isWhite);
-        Board bestMove;
+        Board bestMove = null;
         double bestMoveValue = Double.NEGATIVE_INFINITY;
         for (Board move : possibleMoves) {
-        }
-    }
-    
-    private double[] translateBoard(Board board, boolean isWhite) {
-        // 5 neurons for each board position, and 2 extra for # of pieces left
-        double[] result = new double[5 * 32 + 2];
-        int boardPosition = 0;
-        for (int i = 0; i < 8; ++i) {
-            for (int j = 0; j < 8; ++j) {
+            double[] input = translateBoard(move, isWhite);
+            double value = this.neuralNet.input(input)[0];
+            if (value > bestMoveValue) {
+                bestMove = move;
+                bestMoveValue = value;
             }
         }
+        return bestMove;
+    }
+    
+    private static double[] translateBoard(Board board, boolean isWhite) {
+        // 5 neurons for each board position, and 2 extra for # of pieces left
+        double[] result = new double[5 * 32 + 3];
+        int boardPosition = 0;
+        int friendlyPieces = 0;
+        int enemyPieces = 0;
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                int row = isWhite ? i : 7 - i;
+                int col = isWhite ? j : 7 - j;
+                if (row % 2 == col % 2) {
+                    PieceType piece = board.getPieceAt(row, col);
+                    if (piece == null) {
+                        result[boardPosition * 5] = 1.0;
+                    }
+                    else if (piece == PieceType.WhiteChecker) {
+                        if (isWhite) {
+                            result[boardPosition * 5 + 1] = 1.0;
+                            friendlyPieces += 1;
+                        }
+                        else {
+                            result[boardPosition * 5 + 3] = 1.0;
+                            enemyPieces += 1;
+                        }
+                    }
+                    else if (piece == PieceType.WhiteKing) {
+                        if (isWhite) {
+                            result[boardPosition * 5 + 2] = 1.0;
+                            friendlyPieces += 1;
+                        }
+                        else {
+                            result[boardPosition * 5 + 4] = 1.0;
+                            enemyPieces += 1;
+                        }
+                    }
+                    else if (piece == PieceType.BlackChecker) {
+                        if (isWhite) {
+                            result[boardPosition * 5 + 3] = 1.0;
+                            enemyPieces += 1;
+                        }
+                        else {
+                            result[boardPosition * 5 + 1] = 1.0;
+                            friendlyPieces += 1;
+                        }
+                    }
+                    else if (piece == PieceType.BlackKing) {
+                        if (isWhite) {
+                            result[boardPosition * 5 + 4] = 1.0;
+                            enemyPieces += 1;
+                        }
+                        else {
+                            result[boardPosition * 5 + 2] = 1.0;
+                            friendlyPieces += 1;
+                        }
+                    }
+                }
+                boardPosition += 1;
+            }
+        }
+        result[5 * 32] = friendlyPieces;
+        result[5 * 32 + 1] = enemyPieces;
+        result[5 * 32 + 2] = isWhite ? 1 : -1;
+        return result;
     }
     
     private final NeuralNet neuralNet;
