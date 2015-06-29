27c69208aa94f1b12f9b949bc1cf6dc2af18a97e
duane.byer <duane.byer@gmail.com>
2015-06-29 17:18:32 -0400

    Initial commit

---
+++ src/com/byer/neuralnetcheckers/Main.java
@@ -0,0 +1,16 @@
+package com.byer.neuralnetcheckers;
+
+/**
+ *
+ * @author Duane Byer
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
