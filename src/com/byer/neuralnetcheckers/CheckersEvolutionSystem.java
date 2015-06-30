fae6ff91079a1dba00cd9ba47e74f7581fb4e14c
duane.byer <duane.byer@gmail.com>
2015-06-30 14:43:05 -0400

    Abstracted out evolution system to work on any kind of population

---
+++ src/com/byer/neuralnetcheckers/CheckersEvolutionSystem.java
@@ -0,0 +1,55 @@
+package com.byer.neuralnetcheckers;
+
+import com.asemahle.neuralnet.NeuralNet;
+import com.byer.checkers.Game;
+import java.util.List;
+
+/**
+ *
+ * @author Duane Byer
+ */
+public class CheckersEvolutionSystem extends EvolutionSystem<NeuralNet> {
+
+    public CheckersEvolutionSystem(
+            List<NeuralNet> population,
+            int matchesPerGeneration,
+            double cullRate,
+            double weightMutationRate,
+            double biasMutationRate) {
+        super(population, matchesPerGeneration, cullRate);
+        this.weightMutationRate = weightMutationRate;
+        this.biasMutationRate = biasMutationRate;
+    }
+    
+    @Override
+    public EloRatingSystem.Result playGame(NeuralNet netA, NeuralNet netB) {
+        // Run the game between the two players.
+        NeuralNetPlayer playerA = new NeuralNetPlayer(netA);
+        NeuralNetPlayer playerB = new NeuralNetPlayer(netB);
+
+        Game game = new Game(playerA, playerB, Main.TURN_LIMIT);
+        while (true) {
+            Game.Status status = game.takeNextTurn();
+            if (status == Game.Status.WhiteWin) {
+                return EloRatingSystem.Result.PLAYER_1_WIN;
+            }
+            else if (status == Game.Status.BlackWin) {
+                return EloRatingSystem.Result.PLAYER_2_WIN;
+            }
+            else if (status == Game.Status.Tie) {
+                return EloRatingSystem.Result.TIE;
+            }
+        }
+    }
+
+    @Override
+    public NeuralNet makeChild(NeuralNet parent) {
+        NeuralNet next = parent.copy();
+        next.mutate(this.weightMutationRate, this.biasMutationRate);
+        return next;
+    }
+    
+    private final double weightMutationRate;
+    private final double biasMutationRate;
+    
+}
--- src/com/byer/neuralnetcheckers/EvolutionSystem.java
+++ src/com/byer/neuralnetcheckers/EvolutionSystem.java
@@ -1,7 +1,5 @@
 package com.byer.neuralnetcheckers;
 
-import com.asemahle.neuralnet.NeuralNet;
-import com.byer.checkers.Game;
 import java.util.List;
 import java.util.Random;
 
@@ -9,55 +7,35 @@ import java.util.Random;
  *
  * @author Duane Byer
  */
-public class EvolutionSystem {
+public abstract class EvolutionSystem<Individual> {
     
     public EvolutionSystem(
-            List<NeuralNet> population, 
+            List<Individual> population, 
             int matchesPerGeneration, 
-            double cullRate, 
-            double weightMutationRate, 
-            double biasMutationRate) {
+            double cullRate) {
         
         this.matchesPerGeneration = matchesPerGeneration;
-        this.weightMutationRate = weightMutationRate;
-        this.biasMutationRate = biasMutationRate;
         this.cullRate = cullRate;
         this.ratingSystem = new EloRatingSystem(population);
     }
     
     public double runGeneration() {
         
-        List<NeuralNet> nets = this.getIndividuals();
+        List<Individual> nets = this.getIndividuals();
         
         for (int i = 0; i < this.matchesPerGeneration; ++i) {
-            NeuralNet netA = nets.get(this.random.nextInt(nets.size()));
-            NeuralNet netB = this.ratingSystem.getBestMatch(netA);
+            Individual netA = nets.get(this.random.nextInt(nets.size()));
+            Individual netB = this.ratingSystem.getBestMatch(netA);
             
-            // Run the game between the two players.
-            NeuralNetPlayer playerA = new NeuralNetPlayer(netA);
-            NeuralNetPlayer playerB = new NeuralNetPlayer(netB);
+            EloRatingSystem.Result result = this.playGame(netA, netB);
             
-            Game game = new Game(playerA, playerB, Main.TURN_LIMIT);
-            while (true) {
-                Game.Status status = game.takeNextTurn();
-                if (status == Game.Status.WhiteWin) {
-                    this.ratingSystem.updateRating(netA, netB, EloRatingSystem.Result.PLAYER_1_WIN);
-                    break;
-                }
-                else if (status == Game.Status.BlackWin) {
-                    this.ratingSystem.updateRating(netA, netB, EloRatingSystem.Result.PLAYER_2_WIN);
-                    break;
-                }
-                else if (status == Game.Status.Tie) {
-                    this.ratingSystem.updateRating(netA, netB, EloRatingSystem.Result.TIE);
-                    break;
-                }
-            }
+            this.ratingSystem.updateRating(netA, netB, result);
             
         }
         
         // Generate the next generation by removing underperforming individuals
         // and allowing fit individuals to reproduce.
+        nets = this.getIndividuals();
         int numCulled = (int) (cullRate * nets.size());
         int numChildren = numCulled / (nets.size() - numCulled);
         for (int i = 0; i < numCulled; ++i) {
@@ -65,30 +43,29 @@ public class EvolutionSystem {
         }
         if (numChildren == 0) {
             for (int i = 0; i < numCulled; ++i) {
-                NeuralNet nextNet = nets.get(nets.size() - i - 1).copy();
-                nextNet.mutate(this.weightMutationRate, this.biasMutationRate);
+                Individual nextNet = this.makeChild(nets.get(nets.size() - i - 1));
                 this.ratingSystem.addPlayer(nextNet);
             }
         }
         else {
             for (int i = numCulled; i < nets.size(); ++i) {
-                NeuralNet nextNet = nets.get(i).copy();
-                nextNet.mutate(this.weightMutationRate, this.biasMutationRate);
+                Individual nextNet = this.makeChild(nets.get(i));
                 this.ratingSystem.addPlayer(nextNet, this.ratingSystem.getRating(nets.get(i)));
             }
         }
         return this.ratingSystem.getAverageRating();
     }
     
-    public List<NeuralNet> getIndividuals() {
+    public List<Individual> getIndividuals() {
         return this.ratingSystem.getWorstToBest();
     }
     
+    public abstract EloRatingSystem.Result playGame(Individual netA, Individual netB);
+    public abstract Individual makeChild(Individual parent);
+    
     private final Random random = new Random();
     private final int matchesPerGeneration;
-    private final double weightMutationRate;
-    private final double biasMutationRate;
     private final double cullRate;
-    private EloRatingSystem<NeuralNet> ratingSystem;
+    private EloRatingSystem<Individual> ratingSystem;
     
 }
