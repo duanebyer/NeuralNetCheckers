5b76c45feb4dae7164b73b9899eb28e11a07c23c
duane.byer <duane.byer@gmail.com>
2015-06-29 22:49:44 -0400

    Added a mode for playing against the random bot

---
+++ src/com/byer/neuralnetcheckers/PlayStupidBotMain.java
@@ -0,0 +1,50 @@
+package com.byer.neuralnetcheckers;
+
+import com.asemahle.neuralnet.NeuralNet;
+import com.byer.checkers.Game;
+import com.byer.checkers.HumanPlayer;
+import com.byer.checkers.StupidCheckersPlayer;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Duane Byer
+ */
+public class PlayStupidBotMain {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the file name of the neural net you want to play:");
+        String fileName = scanner.nextLine();
+        NeuralNet[] nets = NeuralNet.loadFromFile(fileName, Main.ACTIVATION_FUNCTION);
+        System.out.println("There are " + nets.length + " nets in this file. Enter the number of the one you want:");
+        int netNumber = scanner.nextInt();
+        int stupidBotWins = 0;
+        int neuralBotWins = 0;
+        while (true) {
+            NeuralNetPlayer netPlayer = new NeuralNetPlayer(nets[netNumber]);
+            Game game = new Game(new StupidCheckersPlayer(), netPlayer, Main.TURN_LIMIT);
+            while (true) {
+                //game.getBoard().print(new PrintWriter(System.out));
+                Game.Status result = game.takeNextTurn();
+                if (result == Game.Status.BlackWin) {
+                    //System.out.println("Neural net wins!");
+                    neuralBotWins += 1;
+                    break;
+                }
+                if (result == Game.Status.WhiteWin) {
+                    //System.out.println("Human wins!");
+                    stupidBotWins += 1;
+                    break;
+                }
+                if (result == Game.Status.Tie) {
+                    //System.out.println("Tie!");
+                    break;
+                }
+            }
+            System.out.println(neuralBotWins + "   " + stupidBotWins);
+        }
+    }
+    
+}
