83200c68e1f88d82d7ac1cae9dd825c8eed74f7d
asemahle <aidan.mahler13@gmail.com>
2015-07-06 09:59:55 -0400

    Small changes, not worth noting

--- src/com/byer/neuralnetcheckers/main/EvolveCheckersNet.java
+++ src/com/byer/neuralnetcheckers/main/EvolveCheckersNet.java
@@ -18,11 +18,12 @@ public class EvolveCheckersNet {
     public static final ActivationFunction ACTIVATION_FUNCTION = (double x) -> x / Math.sqrt(1 + x * x);
     public static final int TURN_LIMIT = 20;
     
+    public static final int POPULATION_SIZE = 100;
     public static final int NUM_HIDDEN_LAYERS = 2;
     public static final int NEURONS_PER_LAYER = 100;
     public static final double CULL_RATE = 0.2;
-    public static final double WEIGHT_MUTATION_RATE = 0.0005;
-    public static final double BIAS_MUTATION_RATE = 0.0005;
+    public static final double WEIGHT_MUTATION_RATE = 0.005;
+    public static final double BIAS_MUTATION_RATE = 0.005;
     /**
      * @param args the command line arguments
      */
@@ -34,7 +35,7 @@ public class EvolveCheckersNet {
         
         NeuralNet[] nets;
         if (fileName.equals("new")) {
-            nets = new NeuralNet[100];
+            nets = new NeuralNet[POPULATION_SIZE];
             for (int i = 0; i < nets.length; ++i) {
                 nets[i] = new NeuralNet(NeuralNetPlayer.NUM_INPUTS, 1, NUM_HIDDEN_LAYERS, NEURONS_PER_LAYER, true, EvolveCheckersNet.ACTIVATION_FUNCTION);
                 nets[i].initConnectionWeights(0.0, 1, 0.0, 0.5);
