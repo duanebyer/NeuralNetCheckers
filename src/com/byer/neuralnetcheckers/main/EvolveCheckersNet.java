1cc5c0cfe8b3036dd2106cc43a2fb2e2ca818cda
asemahle <aidan.mahler13@gmail.com>
2015-07-06 09:10:04 -0400

    Added public static final variables for representing simulation parameters. This is so they can be accessed by TestAndGraphCheckersNet.java

--- src/com/byer/neuralnetcheckers/main/EvolveCheckersNet.java
+++ src/com/byer/neuralnetcheckers/main/EvolveCheckersNet.java
@@ -18,6 +18,11 @@ public class EvolveCheckersNet {
     public static final ActivationFunction ACTIVATION_FUNCTION = (double x) -> x / Math.sqrt(1 + x * x);
     public static final int TURN_LIMIT = 20;
     
+    public static final int NUM_HIDDEN_LAYERS = 2;
+    public static final int NEURONS_PER_LAYER = 100;
+    public static final double CULL_RATE = 0.2;
+    public static final double WEIGHT_MUTATION_RATE = 0.0005;
+    public static final double BIAS_MUTATION_RATE = 0.0005;
     /**
      * @param args the command line arguments
      */
@@ -25,18 +30,20 @@ public class EvolveCheckersNet {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the file name of the neural net population:");
         String fileName = scanner.nextLine();
+        scanner.close();
+        
         NeuralNet[] nets;
         if (fileName.equals("new")) {
             nets = new NeuralNet[100];
             for (int i = 0; i < nets.length; ++i) {
-                nets[i] = new NeuralNet(NeuralNetPlayer.NUM_INPUTS, 1, 2, 100, true, EvolveCheckersNet.ACTIVATION_FUNCTION);
-                nets[i].initConnectionWeights(0.0, 0.6, 0.0, 0.3);
+                nets[i] = new NeuralNet(NeuralNetPlayer.NUM_INPUTS, 1, NUM_HIDDEN_LAYERS, NEURONS_PER_LAYER, true, EvolveCheckersNet.ACTIVATION_FUNCTION);
+                nets[i].initConnectionWeights(0.0, 1, 0.0, 0.5);
             }
         }
         else {
             nets = NeuralNet.loadFromFile(fileName, ACTIVATION_FUNCTION);
         }
-        CheckersEvolutionSystem system = new CheckersEvolutionSystem(Arrays.asList(nets), 0.4, 0.001, 0.001);
+        CheckersEvolutionSystem system = new CheckersEvolutionSystem(Arrays.asList(nets), CULL_RATE, WEIGHT_MUTATION_RATE, BIAS_MUTATION_RATE);
         
         int generation = 1;
         while (true) {
